f2147c1f102923caead088b380f058cb22ff5dbc
PrettyCoffee <66521452+PrettyCoffee@users.noreply.github.com>
2025-09-16 22:01:49 +0200

    feat(NotFound): Improve NotFound route

--- src/app/routes/not-found.tsx
+++ src/app/routes/not-found.tsx
@@ -1,6 +1,10 @@
+import { Frown } from "lucide-react"
+
+import { NoData } from "components/ui/no-data"
+
 export const NotFoundRoute = () => (
-  <div className="flex flex-col items-center font-semibold">
-    <h1>404 - Not Found</h1>
+  <div className="grid size-full place-content-center">
+    <NoData icon={Frown} label="404 - Not Found" />
     <p>Sorry, the page you are looking for does not exist.</p>
   </div>
 )
---
+++ src/components/ui/no-data/no-data.tsx
@@ -0,0 +1,84 @@
+import { PropsWithChildren, ReactNode } from "react"
+
+import { keyframes, css } from "goober"
+import { Ghost } from "lucide-react"
+
+import { Icon } from "components/ui/icon"
+import { IconProp } from "types/base-props"
+import { cn } from "utils/cn"
+
+import { hstack, vstack } from "../../../utils/styles"
+
+const rotate = keyframes`
+  0%, 100% {
+    translate: 1rem;
+    rotate: -10deg;
+    top: 0;
+  }
+  25%, 75% {
+    top: -1rem;
+  }
+  50% {
+    translate: -1rem;
+    rotate: 10deg;
+    top: 0;
+  }
+`
+
+const shadowSlide = keyframes`
+  0%, 100% {
+    transform: translateX(1.5rem);
+    width: 3.25rem;
+    height: 0.5rem;
+    opacity: 0.5;
+  }
+  25%, 75% {
+    width: 3.1rem;
+    opacity: 0.4;
+  }
+  50% {
+    transform: translateX(-1.5rem);
+    width: 3.25rem;
+    height: 0.5rem;
+    opacity: 0.5;
+  }
+`
+
+const shadow = css`
+  animation: ${shadowSlide} 2.5s infinite ease-in-out;
+`
+
+const animate = css`
+  animation: ${rotate} 2.5s infinite ease-in-out;
+`
+
+interface NoDataProps extends IconProp {
+  label: string | ReactNode
+}
+export const NoData = ({
+  icon = Ghost,
+  label,
+  children,
+}: PropsWithChildren<NoDataProps>) => (
+  <div className={cn(vstack({ align: "center", gap: 4 }), "py-4")}>
+    <div
+      className={cn(
+        hstack({ align: "center", justify: "center" }),
+        "relative mb-4 size-20"
+      )}
+    >
+      <Icon
+        icon={icon}
+        color="gentle"
+        className={cn("absolute size-18", animate)}
+      />
+      <div
+        className={cn("absolute -bottom-4 rounded-[50%] bg-black", shadow)}
+      />
+    </div>
+    <span className="block max-w-80 text-center font-bold text-text">
+      {label}
+    </span>
+    {children && <div className={hstack({ gap: 2 })}>{children}</div>}
+  </div>
+)
