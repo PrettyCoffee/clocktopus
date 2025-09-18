63ddb60cefdd878669d2b7fc6b032b9ef0a3a5ce
PrettyCoffee <66521452+PrettyCoffee@users.noreply.github.com>
2025-09-19 00:45:39 +0200

    feat(TimeInput): Add arrow up and down events

--- src/components/ui/time-input/time-input.tsx
+++ src/components/ui/time-input/time-input.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { ChangeEvent, Dispatch, KeyboardEvent, useState } from "react"
 
 import { cn } from "utils/cn"
 import { hstack } from "utils/styles"
@@ -23,21 +23,36 @@ const padTime = (value: string) => {
   }
 }
 
+const getTime = (value: string): [number, number] => {
+  const numbers = getNumbers(value)
+  return [Number(numbers.slice(0, 2)), Number(numbers.slice(2, 4))]
+}
+
 const clampTime = (hours: number, minutes: number): [number, number] => {
   if (hours > 23) return [23, 59]
   if (minutes > 59) return [hours, 59]
   return [hours, minutes]
 }
 
+const twoDigit = (number: number) => number.toString().padStart(2, "0")
+
 const forceTime = (value: string) => {
   const padded = padTime(value)
+  const [hours, minutes] = clampTime(...getTime(padded))
+  return `${twoDigit(hours)}:${twoDigit(minutes)}`
+}
 
-  const [hours, minutes] = clampTime(
-    Number(padded.slice(0, 2)),
-    Number(padded.slice(2, 4))
-  )
+const addMinutes = (time: string, diff: number) => {
+  const [hours, minutes] = getTime(time)
+  const newTime = Math.max(hours * 60 + minutes + diff, 0)
 
-  return `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}`
+  const newMinutes = newTime % 60
+  const newHours = (newTime - newMinutes) / 60
+  return forceTime(`${twoDigit(newHours)}${twoDigit(newMinutes)}`)
+}
+
+interface TimeInputProps extends Omit<InputProps, "type" | "onChange"> {
+  onChange?: Dispatch<string>
 }
 
 export const TimeInput = ({
@@ -48,8 +63,48 @@ export const TimeInput = ({
   value,
   className,
   ...props
-}: Omit<InputProps, "type">) => {
+}: TimeInputProps) => {
   const [text, setText] = useState(getNumbers(value))
+
+  const handleChange = (
+    value: string,
+    event: ChangeEvent<HTMLInputElement>
+  ) => {
+    const target = event.currentTarget
+    const start = target.selectionStart
+    const inserted =
+      start === 5 ? value.slice(0, 3) + value.slice(4) : value.slice(0, 4)
+
+    setText(inserted)
+    onChange?.(forceTime(inserted))
+    target.value = inserted
+    target.setSelectionRange(start, start)
+  }
+
+  const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
+    onKeyDown?.(event)
+
+    const key = event.key
+    if (key.length === 1) {
+      const isNumber = /\d/.test(event.key)
+      if (!isNumber) return event.preventDefault()
+      return
+    }
+    if (key === "ArrowUp") {
+      const time = addMinutes(value ?? "", 15)
+      onChange?.(time)
+      setText(getNumbers(time))
+      event.preventDefault()
+      return
+    }
+    if (key === "ArrowDown") {
+      const time = addMinutes(value ?? "", -15)
+      onChange?.(time)
+      setText(getNumbers(time))
+      event.preventDefault()
+      return
+    }
+  }
 
   return (
     <div className={cn("relative w-15 rounded-md", className)}>
@@ -69,23 +124,8 @@ export const TimeInput = ({
         type="text"
         className="w-full px-0 text-center not-focus-visible:text-transparent"
         value={text}
-        onKeyDown={event => {
-          onKeyDown?.(event)
-          if (event.key.length !== 1) return
-          const isNumber = /\d/.test(event.key)
-          if (!isNumber) return event.preventDefault()
-        }}
-        onChange={(value, event) => {
-          const target = event.currentTarget
-          const start = target.selectionStart
-          const inserted =
-            start === 5 ? value.slice(0, 3) + value.slice(4) : value.slice(0, 4)
-
-          setText(inserted)
-          onChange?.(forceTime(inserted), event)
-          target.value = inserted
-          target.setSelectionRange(start, start)
-        }}
+        onKeyDown={handleKeyDown}
+        onChange={handleChange}
         onFocus={event => {
           event.currentTarget.focus()
           event.currentTarget.select()
