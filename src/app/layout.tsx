25e84187e23c255e5a4ccb59650adfd5040acbf6
PrettyCoffee <66521452+PrettyCoffee@users.noreply.github.com>
2025-09-20 01:49:25 +0200

    feat(AppLayout): Add links to side menu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,16 @@
 import { PropsWithChildren, Suspense } from "react"
 
-import { ArrowLeft, Settings } from "lucide-react"
+import { ArrowLeft, History, Settings } from "lucide-react"
 import { useHashLocation } from "wouter/use-hash-location"
 
 import { MainErrorFallback } from "components/errors/main"
 import { Layout } from "components/layouts/layout"
+import { Github } from "components/ui/icon"
+import { IconButton } from "components/ui/icon-button"
 import { Spinner } from "components/ui/spinner"
 import { ErrorBoundary } from "components/utility/error-boundary"
+import { cn } from "utils/cn"
+import { hstack } from "utils/styles"
 
 export const AppLayout = ({ children }: PropsWithChildren) => {
   const [path] = useHashLocation()
@@ -26,6 +30,16 @@ export const AppLayout = ({ children }: PropsWithChildren) => {
           src="./clocktopus.png"
           alt="logo"
         />
+        <div className="flex-1" />
+        <div className={cn(hstack({ gap: 1 }), "ml-4 *:flex-1")}>
+          <IconButton
+            icon={Github}
+            title="Github Repository"
+            href="https://github.com/PrettyCoffee/clocktopus"
+            target="_blank"
+          />
+          <IconButton icon={History} title="Changelog" />
+        </div>
       </Layout.Side>
       <Layout.Main>
         <Suspense fallback={<Spinner size="xl" />}>
