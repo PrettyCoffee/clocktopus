1e0f02fa0b49a9323e52950d3a287e7d071dfe7c
PrettyCoffee <66521452+PrettyCoffee@users.noreply.github.com>
2025-09-28 19:15:17 +0200

    feat(input): Skip grid navigation with left / right arrow if needed

--- src/components/ui/input/input.tsx
+++ src/components/ui/input/input.tsx
@@ -40,17 +40,35 @@ export type InputProps = RefProp<HTMLInputElement> &
     onBlur?: FocusEventHandler<HTMLInputElement>
   }
 
+const handleGridNavigation = (event: KeyboardEvent<HTMLInputElement>) => {
+  const { currentTarget: input, key } = event
+
+  const start = input.selectionStart
+  const end = input.selectionEnd
+  const isFirst = start === end && start === 0
+  const isLast = start === end && start === input.value.length
+
+  if ((key === "ArrowLeft" && !isFirst) || (key === "ArrowRight" && !isLast)) {
+    event.skipGridNavigation = true
+  }
+}
+
 export const Input = ({
   ref,
   alert,
   onChange,
   className,
+  onKeyDown,
   ...props
 }: InputProps) => (
   <input
     ref={ref}
     {...props}
     onChange={event => onChange?.(event.currentTarget.value, event)}
+    onKeyDown={event => {
+      handleGridNavigation(event)
+      onKeyDown?.(event)
+    }}
     className={cn(
       "h-10 rounded-md px-3 text-sm text-text outline-none placeholder:text-text-gentle",
       "border border-stroke-gentle invalid:border-alert-error hover:border-stroke focus-visible:border-stroke-focus",
