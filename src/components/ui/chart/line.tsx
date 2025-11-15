cd2c99723e048cd438b72b6ea599a40a87a64b2f
PrettyCoffee <66521452+PrettyCoffee@users.noreply.github.com>
2025-11-15 20:06:47 +0100

    feat(Chart.Line): Add draw animation

--- src/components/ui/chart/line.tsx
+++ src/components/ui/chart/line.tsx
@@ -3,6 +3,21 @@ import { cn } from "utils/cn"
 import { chartColor, ChartStrokeColor } from "./fragments/chart-color"
 import { Coordinate, useChartContext } from "./fragments/chart-context"
 
+const drawStroke = (node: SVGPolylineElement | null) => {
+  if (!node) return
+  const length = node.getTotalLength()
+  node.style.transitionProperty = ""
+  node.style.transitionDuration = "0s"
+  node.style.strokeDashoffset = String(length)
+  node.style.strokeDasharray = String(length)
+
+  window.queueMicrotask(() => {
+    node.style.transitionProperty = "stroke-dashoffset"
+    node.style.transitionDuration = "0.5s"
+    node.style.strokeDashoffset = "0"
+  })
+}
+
 export interface LineProps extends ChartStrokeColor {
   points: Coordinate[]
 }
@@ -13,12 +28,16 @@ export const Line = ({ points, color = "priority" }: LineProps) => {
 
   return (
     <polyline
+      ref={element => {
+        drawStroke(element)
+      }}
+      points={projectedPoints.map(({ x, y }) => `${x},${y}`).join(" ")}
+      strokeWidth={1}
       className={cn(
+        "ease-out",
         chartColor.stroke({ color }),
         chartColor.fill({ color: "none" })
       )}
-      points={projectedPoints.map(({ x, y }) => `${x},${y}`).join(" ")}
-      strokeWidth={1}
     />
   )
 }
