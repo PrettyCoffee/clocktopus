aa674f59e40c834916a2d1744281be9d252425e6
PrettyCoffee <66521452+PrettyCoffee@users.noreply.github.com>
2025-11-15 23:28:27 +0100

    refactor(Chart.Line): Extract css transition helper

--- src/components/ui/chart/line.tsx
+++ src/components/ui/chart/line.tsx
@@ -2,21 +2,17 @@ import { cn } from "utils/cn"
 
 import { chartColor, ChartStrokeColor } from "./fragments/chart-color"
 import { Coordinate, useChartContext } from "./fragments/chart-context"
+import { createTransition } from "./utils/get-transition"
 
-const drawStroke = (node: SVGPolylineElement | null) => {
-  if (!node) return
-  const length = node.getTotalLength()
-  node.style.transitionProperty = ""
-  node.style.transitionDuration = "0s"
-  node.style.strokeDashoffset = String(length)
-  node.style.strokeDasharray = String(length)
-
-  window.queueMicrotask(() => {
-    node.style.transitionProperty = "stroke-dashoffset"
-    node.style.transitionDuration = "0.5s"
-    node.style.strokeDashoffset = "0"
-  })
-}
+const { runTransition } = createTransition<SVGPolylineElement>({
+  initStyles: node => ({
+    strokeDashoffset: String(node.getTotalLength()),
+    strokeDasharray: String(node.getTotalLength()),
+  }),
+  targetStyles: {
+    strokeDashoffset: "0",
+  },
+})
 
 export interface LineProps extends ChartStrokeColor {
   points: Coordinate[]
@@ -28,13 +24,12 @@ export const Line = ({ points, color = "priority" }: LineProps) => {
 
   return (
     <polyline
-      ref={element => {
-        drawStroke(element)
+      ref={node => {
+        runTransition({ node })
       }}
       points={projectedPoints.map(({ x, y }) => `${x},${y}`).join(" ")}
       strokeWidth={1}
       className={cn(
-        "ease-out",
         chartColor.stroke({ color }),
         chartColor.fill({ color: "none" })
       )}
---
+++ src/components/ui/chart/utils/get-transition.ts
@@ -0,0 +1,74 @@
+import { CSSProperties } from "react"
+
+import { ease } from "utils/ease"
+
+const DURATION = 1000
+const TIMING_FUNCTION = `cubic-bezier(${ease.out.join(",")})`
+
+type AllowedElement = HTMLElement | SVGElement
+
+const toCssProperty = (jsKey: string) =>
+  jsKey.replaceAll(/[A-Z]/g, ([match]) => "-" + match?.toLowerCase())
+
+const toCssProperties = (styles: CSSProperties) =>
+  Object.keys(styles).map(toCssProperty).join(",")
+
+const applyStyles = (node: AllowedElement, styles: CSSProperties) => {
+  Object.entries(styles).forEach(([key, value]) =>
+    node.style.setProperty(toCssProperty(key), String(value || ""))
+  )
+}
+
+interface TransitionProps<TElement extends AllowedElement> {
+  initStyles: CSSProperties | ((node: TElement) => CSSProperties)
+  targetStyles: CSSProperties | ((node: TElement) => CSSProperties)
+}
+
+interface InitTransitionProps<TElement extends AllowedElement> {
+  items?: number
+  index?: number
+  node: TElement | null
+}
+
+export const createTransition = <TElement extends AllowedElement>({
+  initStyles,
+  targetStyles,
+}: TransitionProps<TElement>) => {
+  const initTransition = (node: TElement) => {
+    const styles =
+      initStyles instanceof Function ? initStyles(node) : initStyles
+
+    applyStyles(node, {
+      ...styles,
+      transitionProperty: "",
+      transitionDuration: "0ms",
+      transitionDelay: "0ms",
+    })
+  }
+
+  const runTransition = (node: TElement, items: number, index: number) => {
+    const itemDelay = (DURATION / (items + 1)) * index
+    const styles =
+      targetStyles instanceof Function ? targetStyles(node) : targetStyles
+
+    applyStyles(node, {
+      ...styles,
+      transitionProperty: toCssProperties(styles),
+      transitionDuration: `${DURATION}ms`,
+      transitionDelay: `${itemDelay}ms`,
+      transitionTimingFunction: TIMING_FUNCTION,
+    })
+  }
+
+  return {
+    runTransition: ({
+      node,
+      items = 1,
+      index = 0,
+    }: InitTransitionProps<TElement>) => {
+      if (!node) return
+      initTransition(node)
+      window.queueMicrotask(() => runTransition(node, items, index))
+    },
+  }
+}
