79edf9a284d2e415aa4888698eb1f70fcc3f854e
PrettyCoffee <66521452+PrettyCoffee@users.noreply.github.com>
2025-11-16 00:46:16 +0100

    feat(Chart.Caption): Add offset prop to dodge yAxis

--- src/components/ui/chart/caption.tsx
+++ src/components/ui/chart/caption.tsx
@@ -2,8 +2,11 @@ import { PropsWithChildren } from "react"
 
 import { Text } from "./fragments/text"
 
-export const Caption = ({ children }: PropsWithChildren) => (
-  <Text x={0} y={16} anchor="start">
+export const Caption = ({
+  offset,
+  children,
+}: PropsWithChildren<{ offset?: { x?: number; y?: number } }>) => (
+  <Text x={offset?.x ?? 0} y={16 + (offset?.y ?? 0)} anchor="start">
     {children}
   </Text>
 )
